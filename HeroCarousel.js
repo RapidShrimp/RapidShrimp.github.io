36388ffe57dcc1574fccce94ca1b1a8c2f120d52
RapidAdam1 <adamshrimpton04@outlook.com>
2024-08-30 21:30:13 +0100

    Hero Image Carousel

--- HeroCarousel.js
+++ HeroCarousel.js
@@ -1,15 +1,14 @@
 window.addEventListener('load',function() {
-
-    NextItem(0);
 });
 
 const ChangeText = document.querySelector('.to-replace-text');
 const ChangeImage = document.querySelector('.to-replace-image');
-
+var CurrentIndex = -1;
 
 const HeroList = {
     "Mech Head" : "images/portfolio/mechhead/combat.png",
-    "Dismember Plugin" : "images/portfolio/OtherSide.png"
+    "Dismemberment Plugin" : "images/portfolio/Skeletal Dismemberment/SkeletalDismemberment.png",
+    "The Other Side" : "images/portfolio/OtherSide.png"
 }
 
 
@@ -18,14 +17,35 @@ function SetText(InText){
 }
 
 function SetImage(ImageURL){
-    ChangeImage.src = ImageURL;
+    //ChangeImage.src = ImageURL;
+    ChangeImage.style.backgroundImage = "url("+ImageURL+")";
+
 }
 
-function NextItem(Index){
 
-    var key = Object.keys(HeroList)[Index];
+function HideHero(){
+    ChangeImage.classList.toggle('active');
+    ChangeText.classList.toggle('active');
+    setTimeout(NextItem(),5000)
+}
+
+function NextItem(){
+    CurrentIndex++;    
+    if(CurrentIndex == Object.keys(HeroList).length){
+        CurrentIndex = 0;
+    }
+    var key = Object.keys(HeroList)[CurrentIndex];
     var value = HeroList[key];
-    SetText(key)
-    SetImage(value)
+    SetText(key);
+    SetImage(value);    
+    console.log(CurrentIndex);
+    setTimeout(RevealHero,5000);
 
 }
+
+function RevealHero(){
+    ChangeImage.classList.toggle('active');
+    ChangeText.classList.toggle('active');
+    setTimeout(HideHero,500);
+}
+NextItem();
